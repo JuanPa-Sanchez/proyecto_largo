7f0a710443931ea34749b0146934ee1811f2c0bb
AlexisAlv30 <goareb235@gmail.com>
2025-09-18 00:10:58 -0600

    clase usuario y estudiante

---
+++ Proyecto.py
@@ -0,0 +1,22 @@
+#clase base usuario
+class Usuario:
+    def __init__(self, id_usuario, nombre, correo, **kwargs):
+        self.id_usuario = id_usuario
+        self.nombre = nombre
+        self.correo = correo
+        #Usa setattr para asignar cualquier otro atributo pasado en kwargs
+        for clave, valor in kwargs.items():
+            setattr(self, clave, valor)
+
+    def mostrar_info(self):
+        return f"{self.id_usuario} - {self.nombre} - {self.correo}"
+
+#clase estudiante hereda de usuario
+class Estudiante(Usuario):
+    def __init__(self, id_usuario, nombre, correo, **kwargs):
+        #Extrae 'carnet' de kwargs antes de pasarlo a la clase base
+        self.carnet = kwargs.pop('carnet', None)
+        super().__init__(id_usuario, nombre, correo, **kwargs)
+
+    def mostrar_info(self):
+        return f"Estudiante: {self.nombre}, Carnet: {self.carnet}"
